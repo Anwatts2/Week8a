b1c1f7340dbab22795afffdbcd26504270356951
Anwatts2 <ashleywatts26@gmail.com>
2024-05-14 22:44:31 -0500

    Week8a

---
+++ array.js
@@ -0,0 +1,18 @@
+// # access 'banana' in the array and print the result
+let fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+console.log(fruits[1]);
+// # replace the element containing 'date' with 'blueberry' and print the new array
+// # replace element here
+console.log(`Updated array: ${'blueberry'}`);
+
+// # access 'banana' in the array and print the result
+let fruit = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+console.log(fruits[1]);
+// # replace the element containing 'date' with 'blueberry' and print the new array
+// # replace element here
+fruits[3] = 'blueberry';
+console.log(`Updated array: ${fruits}`);
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < 10; i+=1) {
+    console.log(1);
+}
+let sum = 0;
+for(i = 0; i> 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    sum +=i
+}
+console.log(sum);
+
+
